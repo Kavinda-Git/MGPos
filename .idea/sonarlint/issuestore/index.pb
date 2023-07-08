6f94956c77b9452a0919bbd1ae95ce0303db4fee
kavinda <you@example.com>
2023-07-08 17:05:00 +0530

    initial implemementation

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,21 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+F
+src/common/Common.java,f\6\f695790c7d067b2add1617c9c43c33ca1a5b5ffc
+D
+src/major/Login.java,a\4\a46a5693a7b6d705aceaa54fb5730de3c5e889be
+P
+ src/database/MySQLconnUtils.java,7\1\7133af4326cf740db19aee4134e7d485198d138c
+Q
+!src/database/ConnectionUtils.java,7\2\723fcfb20d0206341fdeaabdb270b5cef252271c
+]
+-src/controller/AdminViewReportController.java,7\6\76cdd16d9cb2ba839daee57aed81631933e68ba5
+O
+src/functs/ReportGenerator.java,7\7\776c203e158a3a194786d5493111dba65cb116cd
+U
+%src/jaspertemplate/daily_report.jrxml,8\3\83ca7e453f561970658d46793e82358b2a0097e1
+N
+src/database/DBInitialize.java,9\d\9d98a825fb1079cfd8a55863d50a43890f861181
+S
+#src/controller/LoginController.java,d\f\df334df4cf9e0215bcc41bca05dfa078b2d8e1c5
---
+++ src/major/Login.java
@@ -0,0 +1,37 @@
+package major;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Login extends Application{
+
+	//public static Login loginTran =new Login();
+
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+		FXMLLoader root = new FXMLLoader(getClass().getResource("/ui/Page_login.fxml"));
+		
+	//	LoginController logincontroller = root.getController();
+		 
+		
+		Scene scene = new Scene(((Parent) root.load()), 2000,1000);
+		primaryStage.setScene(scene);
+		//make these 2 to false to go with default sizing
+		primaryStage.setResizable(true);
+		primaryStage.setMaximized(true);
+		primaryStage.setTitle("");
+		primaryStage.getIcons().add(new Image("graphic/poslogorect.png"));
+		primaryStage.show();
+		
+	}
+	
+	public static void main(String args[]) {
+		Application.launch(args);
+	}
+}
+
